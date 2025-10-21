622744c35b24c749dfbfc1964ef99e14271d0fd1
Dorian-devw <frank.sinca@tecsup.edu.pe>
2025-10-21 00:32:45 -0500

    feat(entities): Add DenunciaEntity with foreign key

--- ProyectoApp/app/src/main/java/com/kotlin/proyectoapp/data/local/entities/DenunciaEntity.kt
+++ ProyectoApp/app/src/main/java/com/kotlin/proyectoapp/data/local/entities/DenunciaEntity.kt
@@ -1,2 +1,25 @@
 package com.kotlin.proyectoapp.data.local.entities
 
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "denuncias",
+    foreignKeys = [ForeignKey(
+        entity = CandidatoEntity::class,
+        parentColumns = ["id"],
+        childColumns = ["candidatoId"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
+data class DenunciaEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val candidatoId: Int,
+    val tipo: String, // Puede ser "Penal", "Civil", "Electoral"
+    val descripcion: String,
+    val fecha: String,
+    val estado: String, // "En proceso", "Archivado", "Sentenciado"
+    val urlFuente: String
+)
