ef32af721586c983e86855fd3034f5d90f9550f6
Dorian-devw <frank.sinca@tecsup.edu.pe>
2025-10-21 00:45:02 -0500

    feat(model): Add Denuncia.kt domain model

--- ProyectoApp/app/src/main/java/com/kotlin/proyectoapp/domain/model/Denuncia.kt
+++ ProyectoApp/app/src/main/java/com/kotlin/proyectoapp/domain/model/Denuncia.kt
@@ -1,2 +1,11 @@
 package com.kotlin.proyectoapp.domain.model
 
+data class Denuncia(
+    val id: Int = 0,
+    val candidatoId: Int,
+    val tipo: String,
+    val descripcion: String,
+    val fecha: String,
+    val estado: String,
+    val urlFuente: String
+)
