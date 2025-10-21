00a7a1853fd6912fe73409c4bfa732addc04b456
Dorian-devw <frank.sinca@tecsup.edu.pe>
2025-10-21 00:31:05 -0500

    feat(entities): Add ProyectoEntity with foreign key

--- ProyectoApp/app/src/main/java/com/kotlin/proyectoapp/data/local/entities/ProyectoEntity.kt
+++ ProyectoApp/app/src/main/java/com/kotlin/proyectoapp/data/local/entities/ProyectoEntity.kt
@@ -1,2 +1,25 @@
 package com.kotlin.proyectoapp.data.local.entities
 
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "proyectos",
+    foreignKeys = [ForeignKey(
+        entity = CandidatoEntity::class,
+        parentColumns = ["id"],
+        childColumns = ["candidatoId"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
+data class ProyectoEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val candidatoId: Int,
+    val titulo: String,
+    val descripcion: String,
+    val fecha: String,
+    val estado: String, // Puede ser "Aprobado", "En trámite", "Rechazado"
+    val urlFuente: String
+)
