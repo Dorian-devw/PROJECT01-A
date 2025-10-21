f2f92441281d2f924dcf14e6557c88f0178502f9
Dorian-devw <frank.sinca@tecsup.edu.pe>
2025-10-21 00:27:45 -0500

    feat(entities): Add CandidatoEntity with Room annotations

--- ProyectoApp/app/src/main/java/com/kotlin/proyectoapp/data/local/entities/CandidatoEntity.kt
+++ ProyectoApp/app/src/main/java/com/kotlin/proyectoapp/data/local/entities/CandidatoEntity.kt
@@ -1,2 +1,21 @@
 package com.kotlin.proyectoapp.data.local.entities
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "candidatos")
+data class CandidatoEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val nombre: String,
+    val apellidos: String,
+    val cargo: String, // Puede ser "Presidencia" o "Congreso"
+    val partido: String,
+    val region: String,
+    val fotoUrl: String,
+    val biografia: String,
+    val fechaNacimiento: String,
+    val profesion: String,
+    val correo: String,
+    val telefono: String
+)
