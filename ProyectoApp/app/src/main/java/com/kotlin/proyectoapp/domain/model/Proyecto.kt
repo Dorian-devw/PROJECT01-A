27872519e88391a93c3124d3192ec946a33be610
Dorian-devw <frank.sinca@tecsup.edu.pe>
2025-10-21 00:45:39 -0500

    feat(model): Add Proyecto.kt domain model

--- ProyectoApp/app/src/main/java/com/kotlin/proyectoapp/domain/model/Proyecto.kt
+++ ProyectoApp/app/src/main/java/com/kotlin/proyectoapp/domain/model/Proyecto.kt
@@ -1,2 +1,11 @@
 package com.kotlin.proyectoapp.domain.model
 
+data class Proyecto(
+    val id: Int = 0,
+    val candidatoId: Int,
+    val titulo: String,
+    val descripcion: String,
+    val fecha: String,
+    val estado: String,
+    val urlFuente: String
+)
