a3fd07dacc2e775fc43620503998043e2f2aef45
Dorian-devw <frank.sinca@tecsup.edu.pe>
2025-10-21 00:43:59 -0500

    feat(model): Add Candidato domain model

--- ProyectoApp/app/src/main/java/com/kotlin/proyectoapp/domain/model/Candidato.kt
+++ ProyectoApp/app/src/main/java/com/kotlin/proyectoapp/domain/model/Candidato.kt
@@ -1,2 +1,19 @@
 package com.kotlin.proyectoapp.domain.model
 
+data class Candidato(
+    val id: Int = 0,
+    val nombre: String,
+    val apellidos: String,
+    val nombreCompleto: String = "$nombre $apellidos",
+    val cargo: String,
+    val partido: String,
+    val region: String,
+    val fotoUrl: String,
+    val biografia: String,
+    val fechaNacimiento: String,
+    val profesion: String,
+    val correo: String,
+    val telefono: String,
+    val proyectos: List<Proyecto> = emptyList(),
+    val denuncias: List<Denuncia> = emptyList()
+)
