1ca242fe15da62c845d05a18f6ccc3a2143ccee5
Dorian-devw <frank.sinca@tecsup.edu.pe>
2025-10-22 20:04:38 -0500

    feat(viewmodel): Add DetalleViewModelFactory for DI

--- ProyectoApp/app/src/main/java/com/kotlin/proyectoapp/ui/screens/detalle/DetalleViewModelFactory.kt
+++ ProyectoApp/app/src/main/java/com/kotlin/proyectoapp/ui/screens/detalle/DetalleViewModelFactory.kt
@@ -1,2 +1,18 @@
 package com.kotlin.proyectoapp.ui.screens.detalle
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.kotlin.proyectoapp.data.repository.CandidatoRepository
+
+class DetalleViewModelFactory(
+    private val repository: CandidatoRepository,
+    private val candidatoId: Int
+) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(DetalleViewModel::class.java)) {
+            return DetalleViewModel(repository, candidatoId) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
